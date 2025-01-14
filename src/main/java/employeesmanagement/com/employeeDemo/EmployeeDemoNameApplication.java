65f818ccfd4f55349ca162baf54e3d5b363601bc
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-01-14 07:53:59 +0430

    initial commit

---
+++ src/main/java/employeesmanagement/com/employeeDemo/EmployeeDemoNameApplication.java
@@ -0,0 +1,13 @@
+package employeesmanagement.com.employeeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDemoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDemoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/employeesmanagement/com/employeeDemo/EmployeeDemoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package employeesmanagement.com.employeeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDemoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
