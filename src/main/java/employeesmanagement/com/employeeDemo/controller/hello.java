20814bd83e311a80218f61678381559e2b62ec2f
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-01-15 06:45:17 +0430

    fix the controller

--- src/main/java/employeesmanagement/com/employeeDemo/controller/hello.java
+++ src/main/java/employeesmanagement/com/employeeDemo/controller/hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     
     @GetMapping("/hello")
-    public String hello(){
+    public String helloo(){
         return "hello";
     }
 }
