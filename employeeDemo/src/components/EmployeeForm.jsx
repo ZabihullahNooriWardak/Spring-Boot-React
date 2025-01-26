a72f5039f1d2bdf96febe156274ca6e75539ba4d
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-01-26 21:18:15 +0430

    complete some work of update employee

--- employeeDemo/src/components/EmployeeForm.jsx
+++ employeeDemo/src/components/EmployeeForm.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
-import { newEmployee } from "../service/employeeService";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getOneEmployee, newEmployee } from "../service/employeeService";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function EmployeeForm() {
     const [name, setName] = useState("")
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const navigator = useNavigate();
-
+     const {id} = useParams();
+    useEffect(()=>{
+           getOneEmployee(id).then(res=>{
+            setName(res.data.name);
+            setLastName(res.data.lastName);
+            setEmail(res.data.email);
+           })
+    },[id])
     function submitHandler(e) {
         e.preventDefault();
         let formValues = { name, lastName, email }
@@ -29,18 +36,18 @@ export function EmployeeForm() {
             <h1>Create Employee page </h1>
             <form className=" ">
                 <div className="form-group d-flex justify-content-center">
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input name="name" id="name" className="form-control w-50 p-2 " aria-describedby="emailHelp" type="text" placeholder="Enter your Name" value={name} onChange={(e) => setName(e.target.value)}></input>
                 </div>
                 <br></br>
                 <div className="form-group d-flex justify-content-center">
-                    <label for="last_name">Last Name</label>
+                    <label htmlFor="last_name">Last Name</label>
                     <input name="lastName" id="last_name" className="form-control w-50 p-2" type="text" placeholder="Enter your Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}></input>
                 </div>
                 <br></br>
 
                 <div className="form-group d-flex justify-content-center">
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input name="email" className="form-control w-50 p-2 " id="email" type="email" placeholder="Enter your Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 </div>
                 <br></br>
--- employeeDemo/src/service/employeeService.jsx
+++ employeeDemo/src/service/employeeService.jsx
@@ -4,3 +4,4 @@ const  EMPLOYEE_REST_API_BASE_URL = 'http://localhost:8080/api/employee'
 
 export const getAllEmployees = ()=> axios.get(EMPLOYEE_REST_API_BASE_URL);
 export const newEmployee = (employee)=>axios.post(EMPLOYEE_REST_API_BASE_URL,employee)
+export const getOneEmployee = (id)=> axios.get(EMPLOYEE_REST_API_BASE_URL+"/"+id);
