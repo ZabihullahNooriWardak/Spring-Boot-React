43355e663b5107342ac1208b00ca2fb34067c1bb
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-01-27 21:10:09 +0430

    adds modal or entity class for department entity

---
+++ src/main/java/employeesmanagement/com/employeeDemo/entity/Department.java
@@ -0,0 +1,27 @@
+package employeesmanagement.com.employeeDemo.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="departments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+ long id;
+
+ String name ;
+
+ String description;
+
+}
