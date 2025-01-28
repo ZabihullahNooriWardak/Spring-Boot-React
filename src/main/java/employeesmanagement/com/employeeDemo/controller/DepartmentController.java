6baf45916414bc9272e44f0ee73b90e8c98db442
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-01-28 21:01:38 +0430

    adds create method to controller

--- src/main/java/employeesmanagement/com/employeeDemo/controller/DepartmentController.java
+++ src/main/java/employeesmanagement/com/employeeDemo/controller/DepartmentController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import employeesmanagement.com.employeeDemo.dto.DepartmentDto;
+import employeesmanagement.com.employeeDemo.entity.Department;
 import employeesmanagement.com.employeeDemo.impl.DepartmentServiceImpl;
+import employeesmanagement.com.employeeDemo.mapper.DepartmentMapper;
 import lombok.AllArgsConstructor;
 
+import java.net.http.HttpResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/departments")
@@ -18,10 +24,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DepartmentController {
     DepartmentServiceImpl departmentServiceImpl;
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable Long id) {
         DepartmentDto departmentDto = departmentServiceImpl.getDepartment(id);
         return ResponseEntity.ok(departmentDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto departmentDto2 = departmentServiceImpl.createDepartment(departmentDto);
+
+        return new ResponseEntity<>(departmentDto2, HttpStatus.CREATED);
     }
 
 }
